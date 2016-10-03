07b296fffbd8c57682358a86ef2befdc76d6ae62
regstrtn <regstrtn@gmail.com>
2016-10-04 01:41:08 +0530

    cliques in igraph

--- igraph.py
+++ igraph.py
@@ -12,6 +12,8 @@ class igraph:
 		self.vertexlist = set()
 		self._graph = defaultdict(set)
 		self.buildedgelist(mytree, composers)
+		self.getallvertices(composers)
+		self.isclique(self.edgelist)
 
 
 	def getoverlappingcomposers(self, mytree, composer):
@@ -29,6 +31,51 @@ class igraph:
 					self.edgelist.append(edgetuple)
 		pprint(self.edgelist)
 
+	def getvertices(self, edgelist):
+		v = []
+		for edge in edgelist:
+			v.append(edge[0])
+			v.append(edge[1])
+		return list(set(v))
+
+	def getallvertices(self, composers):
+		v = []
+		for composer in composers:
+			v.append(composer[-1])
+		self.vertexlist = set(v)
+		print(self.vertexlist)
+
+	def getmaxclique(self):
+		pass
+
+	def getsubsets(self):
+		pass
+
+	def isclique(self, edgelist):
+		#get list of vertices
+		#check if each edge is present
+		vlist = self.getvertices(edgelist)
+		alledges = self.k_subsets(list(vlist), 2)
+		alltuples = []
+		for t in alledges:	
+			alltuples.append(tuple(t))
+		for t in alltuples:
+			reverset = t[::-1]
+			if(t not in edgelist and reverset not in edgelist): 
+				return(False)
+		return(True)
+	
+	def k_subsets(self, lst, k):
+		if(len(lst)<k):
+			return []
+		if(len(lst) == k):
+			return [lst]
+		if(k==1):
+			return [[i] for i in lst]
+		return self.k_subsets(lst[1:], k) + [[lst[0]]+x for x in self.k_subsets(lst[1:], k-1)]
+		#return self.k_subsets(lst[1:],k) + map(lambda x:x+[lst[0]],self.k_subsets(lst[1:], k-1))
+
+
 	def printdotfile(self):
 		'''
 		Run this to make graph: dot -Tps myg.dot -o graph1.ps
@@ -39,4 +86,3 @@ class igraph:
 			linestr = edge[0]+"--"+edge[1]+"\n"
 			f.write(linestr)
 		f.write("}")
-
